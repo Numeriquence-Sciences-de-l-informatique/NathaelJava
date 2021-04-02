a040819dd4e97792873328f972d8a9b8083ad238
Nathael Bonnal <kirua48000@gmail.com>
2021-04-02 09:23:32 +0200

    Update for output console for the grid

--- src/main/java/fr/nathael/Algorithme/TriangleDePascal.java
+++ src/main/java/fr/nathael/Algorithme/TriangleDePascal.java
@@ -1,11 +1,16 @@
-package fr.nathael;
+package fr.nathael.Algorithme;
 
 public class TriangleDePascal {
     public static void main(String[] args) {
         // Triangle de nombre de Pascal
         for ( int i = 0; i < 10; i++) {
             for ( int j =0; j <= i; j++) {
-                System.out.print(" "+j*i);
+                if (j*i < 10) {
+                    System.out.print(" "+j*i+ " ");
+                } else {
+                    System.out.print(" "+j*i);
+                }
+
             }
             System.out.println();
         }
